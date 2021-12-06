9afbfa964736ba09bb552b7c8e6453aa7e16d8ab
pannaraimernin <panna.tech311@gmail.com>
2021-12-06 16:23:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// let test = document.getElementById("test");
+
+// test.addEventListener("mouseover", function( event ) {
+//   // highlight the mouseover target
+//   event.target.style.color = "orange";
